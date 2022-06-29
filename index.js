a2d477c6f0ad08a50b99b137d88c1ebe12c73e2c
Flordeliza Mae Canlas <maemaeperlas@gmail.com>
2022-06-29 09:27:35 -0700

    commit tree fa4b208fb03e50364222fe6dccd0db9affd362fd

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat (names) {
+    return cats.push("Ralph");
+}
+
+function destructivelyPrependCat (names) {
+    return cats.unshift("Bob");
+}
+
+function destructivelyRemoveLastCat (names) {
+    return cats.pop("Garfield");
+}
+
+function destructivelyRemoveFirstCat (names) {
+    return cats.shift("Milo");
+}
+
+function appendCat (names) {
+    const newCats = [...cats, "Broom"];
+    return newCats;
+}
+
+function prependCat (names) {
+    const newCats = ["Arnold", ...cats];
+    return newCats;
+}
+
+function removeLastCat (names) {
+    const newCats = cats.slice(0,2);
+    return newCats;
+}
+
+function removeFirstCat (names) {
+    const newCats = cats.slice(1,3);
+    return newCats;
+}
